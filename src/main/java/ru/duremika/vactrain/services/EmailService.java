dc551414ff31c9721d994acbf1d78fedec3f0d35
Богданов Максим Владимирович <MVlaBogdanov@sberbank.ru>
2022-07-19 11:04:02 +0600

    fix email message

--- src/main/java/ru/duremika/vactrain/services/EmailService.java
+++ src/main/java/ru/duremika/vactrain/services/EmailService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import ru.duremika.vactrain.entities.User;
 import ru.duremika.vactrain.entities.VerificationToken;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Service
 public class EmailService {
     private final VerificationTokenService verificationTokenService;
@@ -20,10 +23,11 @@ public class EmailService {
         this.javaMailSender = javaMailSender;
     }
 
-    public void sendConfirmLink(User user) {
+    public void sendConfirmLink(User user) throws UnknownHostException {
         VerificationToken verificationToken = verificationTokenService.findByUser(user);
         if (verificationToken == null) return;
 
+        String hostaddress =  InetAddress.getLocalHost().getHostAddress();
         String token = verificationToken.getToken();
 
         SimpleMailMessage msg = new SimpleMailMessage();
@@ -37,11 +41,11 @@ public class EmailService {
 
                         We are almost done creating your Vactrain account. To use the full potential of your account we kindly ask you to click on the link below to verify your account:
 
-                        http://localhost:8080/activation?token=%s
+                        %sactivation?token=%s
 
 
                         If you did not register for a Vactrain Account, someone may have registered with your information by mistake. Contact Vactrain support for further assistance."""
-                        .formatted(token)
+                        .formatted(hostaddress, token)
         );
         javaMailSender.send(msg);
     }
